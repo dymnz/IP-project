ca1f694a7143e574d67a005aa0026195d168fa1b
vicodin1123 <vicodin1123@gmail.com>
2017-01-15 21:00:58 +0800

    IFFT

--- p2/1/p1_1.m
+++ p2/1/p1_1.m
@@ -8,36 +8,31 @@ oData = ...
  0.86  2.68-0.8j  0.44+8j      5j    ; ...
  2        7          8j     2.8+0.77j];
 
+%% Zero padding
 DataW = size(oData, 2);
 DataH = size(oData, 1);
-
-%% Constants
 C = ceil(log2(DataW));
 D = ceil(log2(DataH));
 M = 2^C;
 N = 2^D;
-
-%% Zero padding
 Data = zeros(N, M);
 Data(1:DataH, 1:DataW) = oData;
 
 %% 2D FFT
-M = size(Data, 2);
-N = size(Data, 1);
+FFTResult = twoDFFT(Data);
 
-colResult = zeros(N, M);
-% Column operaion
-for col = 1 : N
-odData = Data(:, col);
-colResult(:, col) = oneDFFT(odData);
+%% 2D iFFT
+ReData = twoIDFFT(FFTResult);
+
+%% Verify
+Diff = abs(ReData - Data);
+Diff(Diff<1e-5) = 0;
+if isempty(find(Diff))
+    disp('Correct');
+else
+    disp('Wrong');
 end
 
-fftResult = zeros(N, M);
-% Row operaion
-for row = 1 : N
-odData = colResult(row, :);
-fftResult(row, :) = oneDFFT(odData);
-end
 
 
 
